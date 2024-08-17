f48b516e2e78877d3158d38264db02f324abd62c
NiTheoneste <nitheoneste@gmail.com>
2024-08-17 18:57:38 +0200

    login

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from './plugins/element-plus'
 
 const app = createApp(App)
 
 app.use(router)
+.use(ElementPlus)
 
 app.mount('#app')
---
+++ src/views/auth/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <el-form :model="form" label-width="auto" style="max-width: 600px">
+      <el-form-item label="Username">
+        <el-input v-model="username" />
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Log In</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+  </template>
+  
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    }
+  }
+}
+</script>
+
+  <style scoped>
+  .login {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
