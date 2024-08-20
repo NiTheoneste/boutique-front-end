2bb4fc3e262be8a1fc58c25b03f22b7b5ae432bf
NiTheoneste <nitheoneste@gmail.com>
2024-08-20 20:09:37 +0200

    product, create table

--- src/views/products/CreateProduct.vue
+++ src/views/products/CreateProduct.vue
@@ -1,8 +1,46 @@
 <script>
+import { Plus, Search } from '@element-plus/icons-vue'
+export default{
+	data(){
+		return {
+			keyword:"",Search,
+            productForm: {name: "", price: 0, quantity: 0}
+		}
+	}
+}
 </script>
 
 <template>
-    <div>
-        <h2> Create Product</h2>
-    </div>
+	<div>
+		<el-card>
+            <el-form
+                style="max-width: 600px"
+                :model="productForm"
+                label-width="auto"
+                class="demo-ruleForm"
+            >
+                <h5 class="mb-3">Add Product</h5>
+                <el-form-item label="Name">
+                    <el-input v-model="productForm.name" type="text" autocomplete="off" />
+                </el-form-item>
+
+                <el-form-item label="Price">
+                    <el-input v-model="productForm.price" type="number" autocomplete="off" />
+                </el-form-item>
+
+                <el-form-item label="Quantity">
+                    <el-input v-model="productForm.quantity" type="number" autocomplete="off" />
+                </el-form-item>
+                
+                <el-form-item>
+                <el-button type="primary" @click="submitForm(ruleFormRef)">
+                    Submit
+                </el-button>
+                <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+                </el-form-item>
+            </el-form>
+		</el-card>
+	</div>
 </template>
+
+<style lang="scss" scoped></style>
--- src/views/products/ProductsView.vue
+++ src/views/products/ProductsView.vue
@@ -1,8 +1,61 @@
 <script>
+import { Plus, Search } from '@element-plus/icons-vue'
+
+export default{
+	data(){
+		return {
+			keyword:"",
+            Search,
+            Plus,
+           products: this.$store.state.products
+		}
+	}
+}
 </script>
 
 <template>
-    <div>
-        <h2>Products</h2>
-    </div>
+	<div>
+		<v-row align="center" class="my-2">
+			<v-col>
+				<h4 class="font-weight-medium">Products</h4>
+			</v-col>
+            <v-col cols="auto">
+				<el-button @click="$router.push({name: 'createProduct'})" type="primary" :icon="Plus">Add Product</el-button>
+			</v-col>
+		</v-row>
+		<el-card>
+			<template #header>
+				<div class="card-header d-xl-flex align-center justify-space-between">
+					<div></div>
+					<div class="d-md-flex align-center">
+						<div class="mr-sm-2 my-2 my-sm-0">
+							<el-input v-model="keyword" placeholder="Chercher" class="input-with-select w-100">
+								<template #append>
+									<el-button type="primary" :icon="Search" />
+								</template>
+							</el-input>
+						</div>
+					</div>
+				</div>
+			</template>
+            <el-table :data="products" style="width: 100%">
+                <el-table-column fixed prop="date" label="Date" width="150" />
+                <el-table-column prop="name" label="Name" width="120" />
+                <el-table-column prop="state" label="State" width="120" />
+                <el-table-column prop="city" label="City" width="120" />
+                <el-table-column prop="address" label="Address" width="600" />
+                <el-table-column prop="zip" label="Zip" width="120" />
+                <el-table-column fixed="right" label="Operations" min-width="120">
+                <template #default>
+                    <el-button link type="primary" size="small" @click="handleClick">
+                    Detail
+                    </el-button>
+                    <el-button link type="primary" size="small">Edit</el-button>
+                </template>
+                </el-table-column>
+            </el-table>
+		</el-card>
+	</div>
 </template>
+
+<style lang="scss" scoped></style>
